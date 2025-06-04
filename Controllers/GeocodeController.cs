bc28a513919409e46f6f1368acfae0ea69b44344
madhura.p <madhura.p@IN-SLI7300-PC22>
2025-06-04 15:36:27 +0530

    Add project files.

---
+++ Controllers/GeocodeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PTVGeocodingDemo.Models;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PTVGeocodingDemo.Controllers
+{
+    public class GeocodeController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey;
+
+        public GeocodeController(IConfiguration configuration)
+        {
+            _httpClient = new HttpClient();
+            _apiKey = configuration["PTV:ApiKey"]; // get key from appsettings
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(GeocodeRequest model)
+        {
+            string requestUrl = $"https://api.myptv.com/geocoding/v1/locations/by-text?searchText={model.Address}";
+
+            _httpClient.DefaultRequestHeaders.Clear();
+            _httpClient.DefaultRequestHeaders.Add("apiKey", _apiKey);
+
+            var response = await _httpClient.GetAsync(requestUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                using var doc = JsonDocument.Parse(json);
+                var location = doc.RootElement.GetProperty("locations")[0].GetProperty("referencePosition");
+
+                model.Latitude = location.GetProperty("latitude").ToString();
+                model.Longitude = location.GetProperty("longitude").ToString();
+            }
+
+            return View(model);
+        }
+    }
+}
+
---
+++ Models/GeocodeRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace PTVGeocodingDemo.Models
+{
+    public class GeocodeRequest
+    {
+        public string Address { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+}
